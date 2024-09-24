42135286e62628a4d970f8f120116321423bd6f1
matildetex <matilde.teixeira@ua.pt>
2024-09-24 19:32:59 +0100

    transformation tolower with accents working

--- proj1/textprocessor.cpp
+++ proj1/textprocessor.cpp
@@ -2,64 +2,69 @@
 #include <fstream>
 #include <vector>
 #include <iostream>
-#include <cctype>
 #include <algorithm> 
 #include <clocale>
+#include <cwctype>
 
 
 using namespace std;
 
 class TextProcessor{
     private: 
-        vector<string> textContent;
-        string line;
-        ifstream fin;
+        vector<wstring> textContent;
+        wstring line;
+        wifstream fin;
 
     public:
         bool readFile(const string& filename){
+            locale loc("");
+            fin.imbue(loc);
             fin.open(filename);
             if(!fin.is_open()){
-                cerr << "Error opening file" << endl;
+                wcerr << "Error opening file" << endl;
                 return false;
             }
             while (getline(fin, line)) {  // store content
                textContent.push_back(line);
-               cout << line << endl;
+               wcout << line << endl;
             }
             fin.close();
             return true;
         }
 
         bool applyTransformation(){
+            locale loc(""); 
 
             for (auto& line : textContent) {
                 transform(line.begin(), line.end(), line.begin(),
-                    [](unsigned char c){ 
-                        return tolower(c);
+                    [&loc](wchar_t c) { 
+                        return use_facet<ctype<wchar_t>>(loc).tolower(c);
                     }
                 );
             }
 
             for ( auto& line: textContent){
                 line.erase(remove_if(line.begin(), line.end(),
-                                    [](unsigned char c) { return ispunct(c); }),
+                                    [](wchar_t c) { return iswpunct(c); }),
                             line.end());
             }
             return true;
         }
 
         void printContentInVector() const {
-            cout << "\nContents of the file stored in the vector:" << endl;
+            wcout << "\nContents of the file stored in the vector:" << endl;
             for (const auto& line : textContent) {
-                cout << line << endl; 
+                wcout << line << endl; 
             }
         }
 
 };
 int main(){
+        locale::global(locale(""));
+
         TextProcessor processor;
         if (processor.readFile("a.txt")) {
-            cout << "\nFile read successfully!" << endl;
+            wcout << "\nFile read successfully!" << endl;
         }
         processor.printContentInVector();
         processor.applyTransformation();
