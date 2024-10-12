5762fbc131c570d01f71bf1d4beb616afb231c07
matildetex <matilde.teixeira@ua.pt>
2024-10-12 16:32:30 +0100

    t4 part2

--- proj1/audioprocessor.cpp
+++ proj1/audioprocessor.cpp
@@ -11,6 +11,7 @@
 #include <SFML/Graphics.hpp>
 #include <thread>
 #include <chrono>
+#include <cmath>
 
 using namespace std;
 
@@ -24,6 +25,8 @@ private:
     vector<sf::Int16> rightChannel;
     vector<sf::Int16> midChannel;
     vector<sf::Int16> sideChannel;
+    vector<sf::Int16> targetChannel;
+    vector<sf::Int16> quantizedSamples;
     unsigned int channelCount;
 
 public:
@@ -101,7 +104,8 @@ public:
         }
     }
 
-    void getLeftRightChannels() {
+    void getLeftRightChannels() 
+    {
         channelCount = buffer.getChannelCount();
         sf::Uint64 sampleCount = buffer.getSampleCount();
 
@@ -115,7 +119,8 @@ public:
         }
     }
 
-    void getMIDchannel(){
+    void getMIDchannel()
+    {
         for (size_t i = 0; i < leftChannel.size(); ++i) {
             int mid = (leftChannel[i] + rightChannel[i]) / 2;
             int side = (leftChannel[i] - rightChannel[i]) / 2;
@@ -124,62 +129,87 @@ public:
         }
     }
 
-    void plotHistogram(){
-        if(channelCount >1){
-            getLeftRightChannels();
-            getMIDchannel();
-            vector<int> histogram(256, 0);
-            int binSize = 256;  // Adjust based on 16-bit sample values
+    void plotHistogram(bool quantized = false)
+    {
+        if(quantized){
+            targetChannel=quantizedSamples;
+        }
+        else{
+            if (channelCount > 1) {
+                getLeftRightChannels();
+                getMIDchannel();
+                targetChannel = midChannel; 
+            } else {
+                cerr << "Error: Channel count is not stereo." << endl;
+                return;
+            }
+        }
+        vector<int> histogram(256, 0);
+        int binSize = 256;  // Handle 16-bit sample values
 
-            sf::Color color = sf::Color::Red;
-            int windowWidth = 600;
-            int windowHeight = 600;
-            int binWidth = 3;
-            int maxHeight = 400;
+        sf::Color color = sf::Color::Red;
+        int windowWidth = 600;
+        int windowHeight = 600;
+        int binWidth = 3;
+        int maxHeight = 400;
 
-            sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Audio Histogram");
+        sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), quantized ? "Audio Quantization Histogram" : "Audio Histogram");
 
-            for (auto sample : midChannel) {
-                int binIndex = (sample + 32768) / binSize;  // Adjust to handle signed 16-bit samples
-                if (binIndex >= 0 && binIndex < 256) {
-                    histogram[binIndex]++;
+        for (auto sample : targetChannel) {
+            int binIndex = (sample + 32768) / binSize;  // Handle signed 16-bit samples
+            if (binIndex >= 0 && binIndex < 256) {
+                histogram[binIndex]++;
+            }
+        }
+
+        int maxBinValue = *max_element(histogram.begin(), histogram.end());
+
+        int totalBinsWidth = histogram.size() * binWidth;
+        int startX = (windowWidth - totalBinsWidth) / 2;  // Center horizontally
+        int startY = windowHeight - 50;  
+        
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) {
+                    window.close();
                 }
             }
 
-            int maxBinValue = *max_element(histogram.begin(), histogram.end());
+            window.clear(sf::Color::Black);
 
-            int totalBinsWidth = histogram.size() * binWidth;
-            int startX = (windowWidth - totalBinsWidth) / 2;  // Center horizontally
-            int startY = windowHeight - 50;  
-            
-            while (window.isOpen()) {
-                sf::Event event;
-                while (window.pollEvent(event)) {
-                    if (event.type == sf::Event::Closed) {
-                        window.close();
-                    }
-                }
+            // Draw histogram bars
+            for (size_t i = 0; i < histogram.size(); ++i) {
+                float binHeight = static_cast<float>(histogram[i]) / maxBinValue * maxHeight;
 
-                window.clear(sf::Color::Black);
-
-                // Draw histogram bars
-                for (size_t i = 0; i < histogram.size(); ++i) {
-                    float binHeight = static_cast<float>(histogram[i]) / maxBinValue * maxHeight;
-
-                    sf::RectangleShape bar(sf::Vector2f(binWidth - 1, -binHeight)); // -binHeight to flip vertically
-                    bar.setPosition(startX + i * binWidth, startY);  
-                    bar.setFillColor(color);
-                    window.draw(bar);
-                }
-
-                window.display();
+                sf::RectangleShape bar(sf::Vector2f(binWidth - 1, -binHeight)); // -binHeight to flip vertically
+                bar.setPosition(startX + i * binWidth, startY);  
+                bar.setFillColor(color);
+                window.draw(bar);
             }
 
+            window.display();
         }
 
     }
 
 
+    void quantization(int n)
+    {
+        int sampleCount = buffer.getSampleCount();  
+        int max_value = *max_element(samples, samples + sampleCount);
+        int min_value = *min_element(samples, samples + sampleCount);
+
+        int numLevels = 1 << n;  
+        float step_size = static_cast<float>(max_value - min_value) / (numLevels - 1);
+
+        quantizedSamples.resize(sampleCount); 
+
+        for (size_t i = 0; i < sampleCount; ++i) {
+            float normalizedSample = (samples[i] - min_value) / step_size;
+            quantizedSamples[i] = static_cast<sf::Int16>(round(normalizedSample)) * step_size + min_value;
+        }
+    }
 };
 
 int main(int argc, char *argv[])
@@ -188,7 +218,7 @@ int main(int argc, char *argv[])
       string filename;
     if (argc != 2)
     {
-        filename = "audioprocessor_files/yoyok.wav";  
+        filename = "audioprocessor_files/sample04.wav";  
     }
     else
     {
@@ -205,5 +235,7 @@ int main(int argc, char *argv[])
     p.getAudioInfo();
     p.plotAudioWaveform();
     p.plotHistogram();
+    p.quantization(8);
+    p.plotHistogram(true);
     return 0;
 }
