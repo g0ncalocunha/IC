c25759c4ca2dcb8d497c0f21474f6937957fab7b
matildetex <matilde.teixeira@ua.pt>
2024-10-12 16:03:01 +0100

    t2 and t3-part2

--- proj1/audioprocessor.cpp
+++ proj1/audioprocessor.cpp
@@ -8,6 +8,9 @@
 #include <codecvt>
 #include <locale>
 #include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
@@ -16,6 +19,12 @@ class AudioProcessor
 private:
     sf::SoundBuffer buffer;
     sf::Sound sound;
+    const sf::Int16* samples;
+    vector<sf::Int16> leftChannel;
+    vector<sf::Int16> rightChannel;
+    vector<sf::Int16> midChannel;
+    vector<sf::Int16> sideChannel;
+    unsigned int channelCount;
 
 public:
     bool readFile(const string &file)
@@ -32,17 +41,17 @@ public:
             cerr << "Error loading file" << endl;
             return false;
         }
+        sound.setBuffer(buffer);
         return true;
     }
 
     void playAudio()
     {
-        sf::Sound sound;
-        sound.setBuffer(buffer);
         sound.play();
         while (sound.getStatus() == sf::Sound::Playing)
         {
             // wait for sound to finish
+            this_thread::sleep_for(chrono::milliseconds(100));
         }
     }
 
@@ -55,30 +64,146 @@ public:
         cout << "Channel count: " << buffer.getChannelCount() << endl;
     }
 
-    void plotAudio()
+    void plotAudioWaveform()
     {
-        //plot audio data
-        const sf::Int16 *samples = buffer.getSamples();
-        size_t count = buffer.getSampleCount();
-        for (size_t i = 0; i < count; i++)
-        {
-            cout << samples[i] << " ";
+        samples = buffer.getSamples();
+        sf::Uint64 sampleCount = buffer.getSampleCount();
+        unsigned int channelCount = buffer.getChannelCount();
+        sf::Uint64 samplesPerChannel = sampleCount / channelCount;
+        
+        sf::RenderWindow window(sf::VideoMode(1920, 600), "Audio's Waveform");
+
+        vector<sf::VertexArray> waveforms(channelCount, sf::VertexArray(sf::LinesStrip, samplesPerChannel));
+        float scaleX = static_cast<float>(window.getSize().x) / samplesPerChannel;
+        float scaleY = window.getSize().y / (2.0f * channelCount);
+
+        for (unsigned int channel = 0; channel < channelCount; ++channel) {
+            for (sf::Uint64 i = 0; i < samplesPerChannel; ++i) {
+                float x = i * scaleX;
+                float y = (samples[i * channelCount + channel] / 32768.0f) * scaleY;
+                waveforms[channel][i].position = sf::Vector2f(x, (channel + 0.5f) * (window.getSize().y / channelCount) + y);
+                waveforms[channel][i].color = sf::Color(50 * (channel + 1), 50 * (channel + 2), 250); // Different color for each channel
+            }
+        }
+
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) {
+                    window.close();
+                }
+            }
+            window.clear(sf::Color::Transparent);
+            for (const auto& waveform : waveforms) {
+                window.draw(waveform);
+            }
+            window.display();
         }
     }
+
+    void getLeftRightChannels() {
+        channelCount = buffer.getChannelCount();
+        sf::Uint64 sampleCount = buffer.getSampleCount();
+
+        if (channelCount == 2) {
+            for (sf::Uint64 i = 0; i < sampleCount; i += 2) {
+                leftChannel.push_back(samples[i]);     
+                rightChannel.push_back(samples[i + 1]);
+            }
+        } else {
+            cerr << "Audio is not stereo." << endl;
+        }
+    }
+
+    void getMIDchannel(){
+        for (size_t i = 0; i < leftChannel.size(); ++i) {
+            int mid = (leftChannel[i] + rightChannel[i]) / 2;
+            int side = (leftChannel[i] - rightChannel[i]) / 2;
+            midChannel.push_back(mid);
+            sideChannel.push_back(side);
+        }
+    }
+
+    void plotHistogram(){
+        if(channelCount >1){
+            getLeftRightChannels();
+            getMIDchannel();
+            vector<int> histogram(256, 0);
+            int binSize = 256;  // Adjust based on 16-bit sample values
+
+            sf::Color color = sf::Color::Red;
+            int windowWidth = 600;
+            int windowHeight = 600;
+            int binWidth = 3;
+            int maxHeight = 400;
+
+            sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Audio Histogram");
+
+            for (auto sample : midChannel) {
+                int binIndex = (sample + 32768) / binSize;  // Adjust to handle signed 16-bit samples
+                if (binIndex >= 0 && binIndex < 256) {
+                    histogram[binIndex]++;
+                }
+            }
+
+            int maxBinValue = *max_element(histogram.begin(), histogram.end());
+
+            int totalBinsWidth = histogram.size() * binWidth;
+            int startX = (windowWidth - totalBinsWidth) / 2;  // Center horizontally
+            int startY = windowHeight - 50;  
+            
+            while (window.isOpen()) {
+                sf::Event event;
+                while (window.pollEvent(event)) {
+                    if (event.type == sf::Event::Closed) {
+                        window.close();
+                    }
+                }
+
+                window.clear(sf::Color::Black);
+
+                // Draw histogram bars
+                for (size_t i = 0; i < histogram.size(); ++i) {
+                    float binHeight = static_cast<float>(histogram[i]) / maxBinValue * maxHeight;
+
+                    sf::RectangleShape bar(sf::Vector2f(binWidth - 1, -binHeight)); // -binHeight to flip vertically
+                    bar.setPosition(startX + i * binWidth, startY);  
+                    bar.setFillColor(color);
+                    window.draw(bar);
+                }
+
+                window.display();
+            }
+
+        }
+
+    }
+
+
 };
 
 int main(int argc, char *argv[])
 {
     AudioProcessor p;
+      string filename;
     if (argc != 2)
     {
-        p.readFile("a.wav");
+        filename = "audioprocessor_files/yoyok.wav";  
     }
     else
     {
-        p.readFile(argv[1]);
+        filename = argv[1];
     }
+
+    if (!p.readFile(filename))
+    {
+        cerr << "Failed to load audio file." << endl;
+        return -1;
+    }
+
     p.playAudio();
     p.getAudioInfo();
+    p.plotAudioWaveform();
+    p.plotHistogram();
     return 0;
 }
