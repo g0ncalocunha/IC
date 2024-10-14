f3070cd178f0bbbda5a6868cbd10e0ac03eba2b7
Gonçalo <goncalo.cunha@ua.pt>
2024-10-14 16:18:36 +0100

    added cmake

---
+++ proj1/CMakeFiles/audioProcesor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/audioProcesor.dir/audioprocessor.cpp.o"
+  "CMakeFiles/audioProcesor.dir/audioprocessor.cpp.o.d"
+  "audioProcesor"
+  "audioProcesor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/audioProcesor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- proj1/imageprocessor.cpp
+++ proj1/imageprocessor.cpp
@@ -1,9 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <opencv2/highgui/highgui.hpp>
 
-
+using namespace cv;
 using namespace std;
+
+class ImageProcessor
+{
+  private:
+    wifstream fin;
+
+  public:
+
+    bool showImage(const string &filename)
+    {
+      Mat image = imread(filename);
+      imshow("Original image",image);
+      waitKey(0);
+
+      return true;
+    }
+    // bool splitImage(const Mat image)
+    // {
+      
+    // }
+};
 
 int main(int argc, char const *argv[])
 {
-  /* code */
+  ImageProcessor processor;
+  processor.showImage("imageprocessor_files/demons.jpg");
   return 0;
 }
