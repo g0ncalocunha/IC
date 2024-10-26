383034abffcb39a6981bcdc5d33d782e3bf0130a
Gonçalo <goncalo.cunha@ua.pt>
2024-10-26 01:05:38 +0100

    final

---
+++ proj1/build/CMakeFiles/textProcessor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/textProcessor.dir/textprocessor.cpp.o"
+  "CMakeFiles/textProcessor.dir/textprocessor.cpp.o.d"
+  "textProcessor"
+  "textProcessor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/textProcessor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
